d089ff7a122d6d47ccc9b8d16bb5642947941079
pupp3tbr4 <34111982+pupp3tbr4@users.noreply.github.com>
2024-02-09 21:29:45 +0100

    Create pingsweep.sh
    Sript para crear un barrido de ping, es util para descubrir maquinas  onectadas a la red.
    
    (No olvidar especificar los primeros octetos de la red xxx.xxx.xxx)

---
+++ pingsweep.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+
+
+if [ -z "$1" ]; then
+    echo "Por favor introduce los primeros octetos xxx.xxx.xxx"   
+exit 1
+
+else
+
+for i in $(seq 1 254) ;do (ping -c 1 $1.$i | grep "bytes from" &) ; 
+done
+
+fi
+
+
